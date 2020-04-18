17c384c3e3912e1f13f00adb5b6e8eb956376e3a
Griretrilr <griretrilr@gmail.com>
2020-04-18 23:16:25 +1000

    - dice string conversion, test fixes

--- game_logic/src/dice.rs
+++ game_logic/src/dice.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use rand::Rng;
+use std::fmt;
 
 pub struct Dice {
     size: i32,
@@ -35,6 +36,24 @@ impl Dice {
     }
 }
 
+impl fmt::Display for Dice {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        if self.modifier() > 0 {
+            write!(f, "d{:?}+{:?}", self.size(), self.modifier())
+        } else if self.modifier() < 0 {
+            write!(f, "d{:?}{:?}", self.size(), self.modifier())
+        } else {
+            write!(f, "d{:?}", self.size())
+        }
+    }
+}
+
+impl fmt::Debug for Dice {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Display::fmt(&self, f)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     extern crate rand;
@@ -47,27 +66,56 @@ mod tests {
         SeedableRng::seed_from_u64(0)
     }
 
-    fn test_case(size: i32, modifier: i32, expected_min: i32, expected_max: i32, rolls: i32) {
+    #[test]
+    fn test_to_string() {
+        assert_eq!(Dice::new(1, 0).to_string(), "d1");
+        assert_eq!(Dice::new(1, 1).to_string(), "d1+1");
+        assert_eq!(Dice::new(1, 42).to_string(), "d1+42");
+        assert_eq!(Dice::new(1, -1).to_string(), "d1-1");
+        assert_eq!(Dice::new(1, -42).to_string(), "d1-42");
+    }
+
+    fn test_roll_values_case(
+        size: i32,
+        modifier: i32,
+        expected_min: i32,
+        expected_max: i32,
+        rolls: i32,
+    ) {
         let dice = Dice::new(size, modifier);
         let mut rng = new_rng();
+        let mut min: i32 = std::i32::MAX;
+        let mut max: i32 = std::i32::MIN;
+
         for _ in 1..rolls {
             let v = dice.roll(&mut rng);
-            assert!(v >= expected_min);
-            assert!(v <= expected_max);
+            min = std::cmp::min(v, min);
+            max = std::cmp::max(v, max);
         }
+
+        assert_eq!(min, expected_min);
+        assert_eq!(max, expected_max);
     }
 
     #[test]
-    fn test() {
+    fn test_roll_values() {
         const ROLLS: i32 = 100;
-        test_case(1, 0, 1, 1, ROLLS);
-        test_case(1, 10, 11, 11, ROLLS);
-        test_case(20, 3, 4, 23, ROLLS);
+        test_roll_values_case(1, 0, 1, 1, ROLLS);
+        test_roll_values_case(1, 10, 11, 11, ROLLS);
+        test_roll_values_case(20, 0, 1, 20, ROLLS);
+        test_roll_values_case(20, 3, 4, 23, ROLLS);
+        test_roll_values_case(20, -3, -2, 17, ROLLS);
     }
 
     #[test]
     #[should_panic(expected = "assertion failed")]
     fn zero_size_asserts() {
         Dice::new(0, 0);
+    }
+
+    #[test]
+    #[should_panic(expected = "assertion failed")]
+    fn negative_size_asserts() {
+        Dice::new(-1, 0);
     }
 }
