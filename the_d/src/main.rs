f49fe73a1420a065c19ecb7276fea7ad87748089
Griretrilr <griretrilr@gmail.com>
2020-04-18 21:09:10 +1000

    pancurses basic

--- the_d/src/main.rs
+++ the_d/src/main.rs
@@ -1,3 +1,24 @@
+extern crate pancurses;
+
+use pancurses::{endwin, initscr, noecho, Input};
+
 fn main() {
-    println!("Hello, world!");
+    let window = initscr();
+    window.printw("Type things, press delete to quit\n");
+    window.refresh();
+    window.keypad(true);
+    noecho();
+    loop {
+        match window.getch() {
+            Some(Input::Character(c)) => {
+                window.addch(c);
+            }
+            Some(Input::KeyDC) => break,
+            Some(input) => {
+                window.addstr(&format!("{:?}", input));
+            }
+            None => (),
+        }
+    }
+    endwin();
 }
