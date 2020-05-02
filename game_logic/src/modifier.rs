c6e257dacf92d56f8074316a648ca26fff06decf
Griretrilr <griretrilr@gmail.com>
2020-05-02 13:16:45 +1000

    - some doc comments

--- game_logic/src/modifier.rs
+++ game_logic/src/modifier.rs
@@ -1,22 +1,27 @@
 use std::cmp::{Eq, Ord, PartialEq, PartialOrd};
 use std::ops::Add;
 
+/// A positive or negative modifier for a value such as a dice roll.
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Modifier(i32);
 
 impl Modifier {
+    /// New modifier with the given value.
     pub fn new(value: i32) -> Modifier {
         Modifier(value)
     }
 
+    /// The zero modifier.
     pub fn zero() -> Modifier {
         Modifier(0)
     }
 
+    /// The signed value of the modifier.
     pub fn value(&self) -> i32 {
         self.0
     }
 
+    /// '-' for negative modifiers, '+' otherwise.
     pub fn sign_char(&self) -> char {
         match self.value() {
             v if v >= 0 => '+',
@@ -24,6 +29,7 @@ impl Modifier {
         }
     }
 
+    /// The absolute value of the modifier.
     pub fn abs_value(&self) -> i32 {
         self.value().abs()
     }
--- game_logic/src/simple_rng.rs
+++ game_logic/src/simple_rng.rs
@@ -1,7 +1,8 @@
 use rand::Rng;
 
-/// A small subset of the sys::rand::Rng trait that we require.
+/// A small subset of the sys::rand::Rng trait that we use. So we can mock it easily.
 pub trait SimpleRng {
+    /// `Rng::gen_range<i32>`
     fn gen_range_i32(&mut self, low: i32, high: i32) -> i32;
 }
 
